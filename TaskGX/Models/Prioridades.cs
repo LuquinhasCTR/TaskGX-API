ecac7587f178d0c461a030a477445301481f2c00
Lucas Castro <169384752+LuquinhasCTR@users.noreply.github.com>
2026-02-04 01:23:55 +0000

    Cria a classe Prioridades.cs.

---
+++ TaskGX/Models/Prioridades.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskGX.API.Models
+{
+    public class Prioridades
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Nome { get; set; } = string.Empty;
+    }
+}
