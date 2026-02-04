4d4146afb1531c333161932661aa417f5aa7d448
Lucas Castro <169384752+LuquinhasCTR@users.noreply.github.com>
2026-02-04 01:24:55 +0000

    Cria a classe PropriedadesController.cs.

--- TaskGX/Controllers/PrioridadesController.cs
+++ TaskGX/Controllers/PrioridadesController.cs
@@ -1,6 +1,34 @@
-﻿namespace TaskGX.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskGX.API.Data;
+using TaskGX.API.DTOs;
+
+namespace TaskGX.API.Controllers
 {
-    public class PrioridadesController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PrioridadesController : ControllerBase
     {
+        private readonly TaskGXContext _context;
+
+        public PrioridadesController(TaskGXContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/prioridades
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PrioridadeDTO>>> GetPrioridades()
+        {
+            var prioridades = await _context.Prioridades
+                .Select(p => new PrioridadeDTO
+                {
+                    ID = p.ID,
+                    Nome = p.Nome
+                })
+                .ToListAsync();
+
+            return prioridades;
+        }
     }
 }
