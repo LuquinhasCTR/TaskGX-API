d81aef99486b62fa8bbca38f4a0cf9fea35eb488
Lucas Castro <169384752+LuquinhasCTR@users.noreply.github.com>
2026-02-04 01:26:23 +0000

    Cria a classe TaskGXContext.cs.

---
+++ TaskGX/Data/TaskGXContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskGX.API.Models;
+
+namespace TaskGX.API.Data
+{
+    public class TaskGXContext : DbContext
+    {
+        public TaskGXContext(DbContextOptions<TaskGXContext> options)
+            : base(options) { }
+
+        public DbSet<Usuarios> Usuarios { get; set; }
+        public DbSet<Listas> Listas { get; set; }
+        public DbSet<Tarefas> Tarefas { get; set; }
+        public DbSet<Prioridades> Prioridades { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Aqui você pode configurar relacionamentos, nomes de tabelas, seeds etc.
+        }
+    }
+}
