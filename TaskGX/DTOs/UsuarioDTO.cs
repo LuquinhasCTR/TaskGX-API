eac2a887021fa7984ccb92b130e971060d5e2edf
Lucas Castro <169384752+LuquinhasCTR@users.noreply.github.com>
2026-02-04 01:23:42 +0000

    Cria a classe UsuarioDTO.cs.

---
+++ TaskGX/DTOs/UsuarioDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskGX.API.DTOs
+{
+    public class UsuarioDTO
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Avatar { get; set; }
+        public bool Ativo { get; set; }
+        public bool EmailVerificado { get; set; }
+    }
+}
