f0714f9b1b533c402550f169cf0e24bca8c6d18f
Lucas Castro <169384752+LuquinhasCTR@users.noreply.github.com>
2026-02-04 01:24:31 +0000

    Cria a classe ListaDTO.cs.

---
+++ TaskGX/DTOs/ListaDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskGX.API.DTOs
+{
+    public class ListaDTO
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string? Cor { get; set; }
+        public bool Favorita { get; set; }
+    }
+}
