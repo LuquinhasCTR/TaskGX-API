7c0e9e920f1e57ed82cc6cee73ee59e5816c058a
Lucas Castro <169384752+LuquinhasCTR@users.noreply.github.com>
2026-02-04 01:23:21 +0000

    Cria a classe Listas.cs.

---
+++ TaskGX/Models/Listas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskGX.API.Models
+{
+    public class Listas
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [ForeignKey("Usuario")]
+        public int UsuarioId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Nome { get; set; } = string.Empty;
+
+        [MaxLength(20)]
+        public string? Cor { get; set; }
+
+        public bool Favorita { get; set; } = false;
+
+        public DateTime DataCriacao { get; set; } = DateTime.Now;
+
+        // Relação opcional com usuário (navigation property)
+        public Usuarios? Usuario { get; set; }
+    }
+}
