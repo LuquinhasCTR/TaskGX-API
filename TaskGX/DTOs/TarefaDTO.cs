f93642d74dfc3a4f25a034089ad77ff238b544f6
Lucas Castro <169384752+LuquinhasCTR@users.noreply.github.com>
2026-02-04 01:24:07 +0000

    Cria a classe TarefaDTO.cs.

---
+++ TaskGX/DTOs/TarefaDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace TaskGX.API.DTOs
+{
+    public class TarefaDTO
+    {
+        public int ID { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        public string? Descricao { get; set; }
+        public string? Tags { get; set; }
+        public bool Concluida { get; set; }
+        public bool Arquivada { get; set; }
+        public DateTime? DataVencimento { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public int? ListaId { get; set; }
+        public string? ListaNome { get; set; }
+        public int? PrioridadeId { get; set; }
+        public string? PrioridadeNome { get; set; }
+    }
+}
