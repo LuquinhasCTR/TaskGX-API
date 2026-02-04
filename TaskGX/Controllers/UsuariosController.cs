86545fd731e9c75e2d6c3552b2b7c25f23261fac
Lucas Castro <169384752+LuquinhasCTR@users.noreply.github.com>
2026-02-04 01:25:29 +0000

    Cria a classe UsuariosController.cs.

--- TaskGX/Controllers/UsuariosController.cs
+++ TaskGX/Controllers/UsuariosController.cs
@@ -1,6 +1,55 @@
-﻿namespace TaskGX.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskGX.API.Models;
+using TaskGX.API.Data;  
+using TaskGX.API.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaskGX.API.Controllers
 {
-    public class UsuarioController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
     {
+        private readonly TaskGXContext _context;
+
+        public UsuariosController(TaskGXContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/usuarios/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UsuarioDTO>> GetUsuario(int id)
+        {
+            var usuario = await _context.Usuarios.FindAsync(id);
+
+            if (usuario == null) return NotFound();
+
+            var usuarioDTO = new UsuarioDTO
+            {
+                Nome = usuario.Nome,
+                Email = usuario.Email,
+                Avatar = usuario.Avatar
+            };
+
+            return usuarioDTO;
+        }
+
+        // POST: api/usuarios
+        [HttpPost]
+        public async Task<ActionResult<UsuarioDTO>> CriarUsuario(Usuarios usuario)
+        {
+            _context.Usuarios.Add(usuario);
+            await _context.SaveChangesAsync();
+
+            var usuarioDTO = new UsuarioDTO
+            {
+                Nome = usuario.Nome,
+                Email = usuario.Email,
+                Avatar = usuario.Avatar
+            };
+
+            return CreatedAtAction(nameof(GetUsuario), new { id = usuario.ID }, usuarioDTO);
+        }
     }
 }
