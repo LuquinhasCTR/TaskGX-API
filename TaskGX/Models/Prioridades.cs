9b8a4e0495a4652e6d017a3d2df9f935b6abbf78
Lucas de Castro Silva <169384752+LuquinhasCTR@users.noreply.github.com>
2026-02-05 16:49:25 +0000

    Anota as Prioridades com atributos de tabela/coluna..

--- TaskGX/Models/Prioridades.cs
+++ TaskGX/Models/Prioridades.cs
@@ -1,14 +1,18 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TaskGX.API.Models
 {
+    [Table("Prioridades")]
     public class Prioridades
     {
         [Key]
+        [Column("ID")]
         public int ID { get; set; }
 
         [Required]
         [MaxLength(50)]
+        [Column("Nome")]
         public string Nome { get; set; } = string.Empty;
     }
 }
