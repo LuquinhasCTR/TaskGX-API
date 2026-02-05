fa827f3b34ed2311f357f117afd49726b92f8c16
Lucas de Castro Silva <169384752+LuquinhasCTR@users.noreply.github.com>
2026-02-05 17:00:23 +0000

    Cria a classe RegistrationDTO.

---
+++ TaskGX/DTOs/RegistrationDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskGX.API.DTOs
+{
+    public class RegistrationDTO
+    {
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Senha { get; set; } = string.Empty;
+        public string ConfirmarSenha { get; set; } = string.Empty;
+    }
+}
