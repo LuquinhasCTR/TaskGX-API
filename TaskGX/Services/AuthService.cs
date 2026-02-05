21614773466732163811287b2a4388a396a0f848
Lucas de Castro Silva <169384752+LuquinhasCTR@users.noreply.github.com>
2026-02-05 16:50:02 +0000

    Cria a classe AuthService.

---
+++ TaskGX/Services/AuthService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using TaskGX.API.Models;
+using TaskGX.API.Repositories;
+using TaskGX.Repositories;
+
+namespace TaskGX.API.Services
+{
+    public class AuthService
+    {
+        }
+    }
+}
