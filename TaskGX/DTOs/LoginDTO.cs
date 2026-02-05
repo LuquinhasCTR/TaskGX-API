b6cb95aeefe2a03eb519939c7e10b344a5a97bc5
Lucas de Castro Silva <169384752+LuquinhasCTR@users.noreply.github.com>
2026-02-05 17:00:07 +0000

    Cria a classe LoginDTO.

---
+++ TaskGX/DTOs/LoginDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskGX.API.DTOs
+{
+    public class LoginDTO
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Senha { get; set; } = string.Empty;
+    }
+}
