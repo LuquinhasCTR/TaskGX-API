9e68cb0150a6c9a1e5fac3622afc1f5cecb6ce21
Lucas de Castro Silva <169384752+LuquinhasCTR@users.noreply.github.com>
2026-02-05 17:02:36 +0000

    Cria a classe RegistrationService.

---
+++ TaskGX/Services/RegistrationService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using TaskGX.API.DTOs;
+using TaskGX.API.Models;
+using TaskGX.API.Repositories;
+
+namespace TaskGX.API.Services
+{
+    public class RegistrationService
+    {
+    }
+}
